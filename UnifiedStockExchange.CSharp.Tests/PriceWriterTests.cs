aab7208cc072f58c1d3b672f1912d69e056e0c27
MatrixRonny <35771215+MatrixRonny@users.noreply.github.com>
2023-05-18 17:18:29 +0300

    Adjusted OpenAPI generate script. Updated C# test project to use OpenAPI SDK project. Made some changes to tests.

--- UnifiedStockExchange.CSharp.Tests/PriceWriterTests.cs
+++ UnifiedStockExchange.CSharp.Tests/PriceWriterTests.cs
@@ -1,6 +1,6 @@
 using System.Text.Json.Nodes;
-using UnifiedStockExchangeSdk.CSharp.Api;
-using UnifiedStockExchangeSdk.CSharp.Model;
+using UnifiedStockExchange.Sdk.CSharp.Api;
+using UnifiedStockExchange.Sdk.CSharp.Model;
 
 namespace UnifiedStockExchange.CSharp.Tests
 {
@@ -8,8 +8,10 @@ namespace UnifiedStockExchange.CSharp.Tests
     public class PriceWriterTests
     {
         [TestMethod]
-        public async Task ConnectAndSendSinglePriceUpdate()
+        public async Task ConnectSendPriceUpdateAndReadHistory()
         {
+            //WARNING: This test may fail if run twice withing the same minute.
+
             ///// Arrange /////
 
             AppSettings appSettings = TestUtility.GetAppSettings();
@@ -26,9 +28,11 @@ namespace UnifiedStockExchange.CSharp.Tests
             ///// Act /////
             await priceWriter.ConnectAsync();
             await priceWriter.SendPriceUpdate(dateTimeNow, price, amount);
+            await priceWriter.SendPriceUpdate(dateTimeNow.AddMinutes(1), price + 100, amount * 1.1M);
 
             List<PriceCandle> priceHistory = historyApi.PriceHistoryGetHistoryDataFromPost(
-                appSettings.ExchangeName, 
+                appSettings.ExchangeName,
+                appSettings.Quote,
                 dateTimeNow, 
                 1, 
                 SampleInterval.OneMinute
@@ -36,9 +40,48 @@ namespace UnifiedStockExchange.CSharp.Tests
 
             ///// Assert /////
 
-            Assert.AreEqual(dateTimeNow, priceHistory[0].Date);
-            Assert.AreEqual(price, priceHistory[0].Open);
-            Assert.AreEqual(amount, priceHistory[0].Volume);
+            Assert.AreEqual(dateTimeNow.TruncateByInterval(SampleInterval.OneMinute), priceHistory[0].Date);
+            Assert.AreEqual(price, (decimal)priceHistory[0].Open);
+            Assert.AreEqual(amount, (decimal)priceHistory[0].Volume);
+        }
+
+        [TestMethod]
+        public async Task ConnectSendPriceWaitCacheAndReadHistory()
+        {
+            //WARNING: This test waits for the server to flush price cache and takes a lot of time to complete.
+
+            ///// Arrange /////
+
+            AppSettings appSettings = TestUtility.GetAppSettings();
+            PriceWriter priceWriter = new PriceWriter(
+                appSettings.UnifiedStockExchangeUrl,
+                appSettings.ExchangeName,
+                appSettings.Quote.ToTradingPair()
+            );
+            PriceHistoryApi historyApi = new PriceHistoryApi(appSettings.UnifiedStockExchangeUrl);
+            DateTime dateTimeNow = DateTime.UtcNow;
+            decimal price = 15000;
+            decimal amount = 50;
+
+            ///// Act /////
+            await priceWriter.ConnectAsync();
+            await priceWriter.SendPriceUpdate(dateTimeNow, price, amount);
+            await priceWriter.SendPriceUpdate(dateTimeNow.AddMinutes(1), price + 100, amount * 1.1M);
+
+            await Task.Delay(TimeSpan.FromSeconds(10 * 60 + 1));
+            List<PriceCandle> priceHistory = historyApi.PriceHistoryGetHistoryDataFromPost(
+                appSettings.ExchangeName,
+                appSettings.Quote,
+                dateTimeNow,
+                1,
+                SampleInterval.OneMinute
+            );
+
+            ///// Assert /////
+
+            Assert.AreEqual(dateTimeNow.TruncateByInterval(SampleInterval.OneMinute), priceHistory[0].Date);
+            Assert.AreEqual(price, (decimal)priceHistory[0].Open);
+            Assert.AreEqual(amount, (decimal)priceHistory[0].Volume);
         }
     }
 }
