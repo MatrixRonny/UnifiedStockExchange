a1c1ac6c3cd500bdf654e25f1e9397a80312f20a
MatrixRonny <ronnyourmatrix@gmail.com>
2023-05-19 23:10:39 +0300

    Added tradingPair when sending price updates. Renamed methods for better clarity. Returned Conflict when there is another WebSocket for the same exchange. Fixed bugs.

--- UnifiedStockExchange/Domain/DataTransfer/PriceUpdateData.cs
+++ UnifiedStockExchange/Domain/DataTransfer/PriceUpdateData.cs
@@ -2,12 +2,12 @@
 {
     public class PriceUpdateData
     {
+        public string TradingPair { get; set; } = null!;
+
         public long Time { get; set; }
 
         public decimal Price { get; set; }
 
         public decimal Amount { get; set; }
-
-        public string TradingPair { get; set; } = null!;
     }
 }
