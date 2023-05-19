7d45f9b578b27d6a74b92231ddff1cfa69c3e1c7
MatrixRonny <35771215+MatrixRonny@users.noreply.github.com>
2023-05-19 20:15:11 +0300

    Change PriceHistoryController and moved request data to body.

---
+++ UnifiedStockExchange/Domain/DataTransfer/PriceHistoryRequest.cs
@@ -0,0 +1,14 @@
+﻿using UnifiedStockExchange.Domain.Enums;
+
+namespace UnifiedStockExchange.Domain.DataTransfer
+{
+    public class PriceHistoryRequest
+    {
+        public string ExchangeName { get; set; } = null!;
+        public string TradingPair { get; set; } = null!;
+        public DateTime? FromDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int CandleSamples { get; set; }
+        public SampleInterval CandleInterval { get; set; }
+    }
+}
