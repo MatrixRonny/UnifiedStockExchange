314878b48923d05ea98e2236387d72dbc3a829a1
MatrixRonny <ronnyourmatrix@gmail.com>
2023-05-21 15:16:37 +0300

    Fixed bug.

---
+++ UnifiedStockExchange/Exceptions/ConstraintViolationException.cs
@@ -0,0 +1,13 @@
+﻿namespace UnifiedStockExchange.Exceptions
+{
+    public class ConstraintViolationException : DataAccessException
+    {
+        public ConstraintViolationException(string? message) : base(message)
+        {
+        }
+
+        public ConstraintViolationException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
