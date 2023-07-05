d86d0f370499b68b9012f3299f81fcf6d7dba885
MatrixRonny <ronnyourmatrix@gmail.com>
2023-07-05 14:30:50 +0300

    Moved generate-api to root folder and included code to start server before generating SDK from OpenAPI.

---
+++ generate-api.bat
@@ -0,0 +1,21 @@
+@echo off
+
+SET ServerPath=UnifiedStockExchange\bin\Debug\net6.0\UnifiedStockExchange.exe
+
+if not exist %ServerPath% (
+	echo Please build UnifiedStockExchange project first.
+	exit 1
+)
+
+SET ASPNETCORE_ENVIRONMENT=Development
+SET AspNetUrls=https://localhost:7288;http://localhost:5099
+SET StartServerCmd=powershell.exe -Command "(Start-Process -FilePath %ServerPath% -Argument \"--urls %AspNetUrls%\" -PassThru -WindowStyle Hidden).Id"
+%StartServerCmd% >temp & (set /p PID=)<temp & del temp
+
+cd /D "%~dp0"
+
+if exist UnifiedStockExchange.Sdk.CSharp rmdir /S /Q UnifiedStockExchange.Sdk.CSharp
+
+openapi-generator generate -i http://localhost:5099/swagger/v1/swagger.json -g csharp-netcore --additional-properties packageName=UnifiedStockExchange.Sdk.CSharp -o UnifiedStockExchange.Sdk.CSharp
+
+taskkill /PID %PID%
