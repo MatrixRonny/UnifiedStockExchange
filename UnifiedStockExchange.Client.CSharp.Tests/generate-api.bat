d059816bb101dcb57c105e054e21b5300b73b4d1
MatrixRonny <35771215+MatrixRonny@users.noreply.github.com>
2023-07-05 14:30:50 +0300

    Moved generate-api to root folder and included code to start server before generating SDK from OpenAPI.

--- UnifiedStockExchange.Client.CSharp.Tests/generate-api.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd /D "%~dp0"
-
-if exist ..\UnifiedStockExchange.Sdk.CSharp rmdir /S /Q ..\UnifiedStockExchange.Sdk.CSharp
-
-openapi-generator generate -i http://localhost:5097/swagger/v1/swagger.json -g csharp-netcore --additional-properties packageName=UnifiedStockExchange.Sdk.CSharp -o ..\UnifiedStockExchange.Sdk.CSharp
